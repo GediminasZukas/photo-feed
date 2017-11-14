8f777863958203a815d0d2a81b58ebe5bd8a24e4
DESKTOP-IV5B30T\funguide <gediminas.zukas@funguide.lt>
2017-11-14 19:49:17 +0200

    Add PhotosDataRepository with data store factory

---
+++ app/src/main/java/com/gapps/photofeed/screens/feed/data/repository/PhotosDataStoreFactory.kt
@@ -0,0 +1,10 @@
+package com.gapps.photofeed.screens.feed.data.repository
+
+import com.gapps.photofeed.network.ApiCallExecutor
+import com.gapps.photofeed.screens.feed.data.sources.cloud.PhotosCloudDataStore
+import javax.inject.Inject
+
+class PhotosDataStoreFactory @Inject constructor(private val apiCallExecutor: ApiCallExecutor) {
+
+    fun createCloudDataStore(): PhotosDataStore = PhotosCloudDataStore(apiCallExecutor)
+}
